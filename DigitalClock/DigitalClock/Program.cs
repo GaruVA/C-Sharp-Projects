2552c4206cb291ea497a7a4d85fa9a1d5b4f56b6
Garuka Veenath Assalaarachchi <garukavassalaarachchi@gmail.com>
2024-02-05 22:13:35 +0530

    To do app basic

---
+++ DigitalClock/DigitalClock/Program.cs
@@ -0,0 +1,51 @@
+﻿using DigitalClock;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+Clock clock = new Clock();
+while (true)
+{
+    clock.UpdateTime();
+    Console.Clear();
+    Console.WriteLine($"Time: {clock.Hours:D2}:{clock.Minutes:D2}:{clock.Seconds:D2}");
+    Thread.Sleep(1000);
+}
+
+namespace DigitalClock
+{
+    class Clock
+    {
+        private int hours;
+        private int minutes;
+        private int seconds;
+
+        public int Hours
+        {
+            get { return hours; }
+            set { hours = value; }
+        }
+
+        public int Minutes
+        {
+            get { return minutes; }
+            set { minutes = value; }
+        }
+
+        public int Seconds
+        {
+            get { return seconds; }
+            set { seconds = value; }
+        }
+
+        public void UpdateTime()
+        {
+            DateTime now = DateTime.Now;
+            Hours = now.Hour;
+            Minutes = now.Minute;
+            Seconds = now.Second;
+        }
+    }
+}
---
+++ ToDoApp/ToDoApp/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ToDoApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("To Do List");
+            Console.WriteLine("------------------------------------");
+            List<string> taskList = new List<string>();
+            string choice = "temp";
+            int taskNum;
+            while (true)
+            {
+                Console.WriteLine("------------------------------------");
+                for (int i = 0; i < taskList.Count; i++) 
+                {
+                    Console.WriteLine($"{taskList[i]}");
+                }
+                Console.WriteLine("------------------------------------");
+                Console.WriteLine("1 - New Task");
+                Console.WriteLine("2 - Delete Task");
+                Console.WriteLine("e - Exit");
+                Console.WriteLine();
+                Console.Write("Enter Choice: ");
+                choice = Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine("------------------------------------");
+                Console.WriteLine("To Do List");
+                Console.WriteLine("------------------------------------");
+                if (choice == "e")
+                {
+                    break;
+                }
+                else if (choice == "1")
+                {
+                    Console.Write("Enter Task: ");
+                    taskList.Add(Console.ReadLine());
+                    Console.WriteLine("Success: Task has been added.");
+                }
+                else if (choice == "2")
+                {
+                    for (int i = 0; i < taskList.Count; i++)
+                    {
+                        Console.WriteLine($"{i + 1} - {taskList[i]}");
+                    }
+                    Console.WriteLine();
+                    Console.Write("Enter Task Number: ");
+                    taskNum = int.Parse(Console.ReadLine()) - 1;
+                    if (taskNum >= 0 && taskNum < taskList.Count)
+                    {
+                        taskList.RemoveAt(taskNum);
+                        Console.WriteLine("Success: Task has been deleted.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Value!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Choice!");
+                }
+                Thread.Sleep(2000);
+                Console.Clear();
+                Console.WriteLine("------------------------------------");
+                Console.WriteLine("To Do List");
+                Console.WriteLine("------------------------------------");
+            }
+        }
+    }
+}
